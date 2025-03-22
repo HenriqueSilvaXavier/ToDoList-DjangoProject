c3d9332262a4c6c17e4f8afc4bf85df81de4fc87
HenriqueSilvaXavier <134643923+HenriqueSilvaXavier@users.noreply.github.com>
2025-03-22 13:58:16 -0300

    Delete toDoList directory

--- toDoList/tarefas/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Tarefa
-
-@admin.register(Tarefa)
-class TarefaAdmin(admin.ModelAdmin):
-    list_display = ('materia', 'descricao', 'prazo', 'criada_em')
